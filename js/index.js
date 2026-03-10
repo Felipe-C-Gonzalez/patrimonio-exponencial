855e8963d9e5730aaaafc67e4e6804e770de6959
Felipe-C-Gonzalez <felipecgonzalez2305@gmail.com>
2026-03-10 10:11:42 -0300

    feat(js): implementar lógica de cálculo e vinculação com o DOM

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const investimentForm = document.getElementById('investiment-form');
+const initialValue = document.getElementById('initial-value');
+const contribution = document.getElementById('contribution');
+const rate = document.getElementById('rate');
+const time = document.getElementById('time');
+
+const btnCalculate = document.getElementById('btn-calculate');
+
+const resultsSection = document.getElementById('results')
+
+investimentForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const initialValueNumber = Number(initialValue.value);
+    const contributionValue = Number(contribution.value);
+    const rateValue = Number(rate.value) / 100;
+    const timeValue = Number(time.value);
+
+    let totalValue = initialValueNumber;
+    for (let i = 1; i <= timeValue; i++) {
+        totalValue = (totalValue + contributionValue) * (1 + rateValue);
+        console.log(totalValue)
+    }
+
+    const totalFormatted = totalValue.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    resultsSection.innerHTML = `O valor total é ${totalFormatted}`
+})
